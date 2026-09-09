8b7830fb52fff3b33c52560309733312cb19d929
蓬蓬貓 <154980876+meowcrafrt@users.noreply.github.com>
2026-09-09 11:28:28 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plt-pattern-tool-v2';
+const CACHE_NAME = 'plt-pattern-tool-v3';
 const APP_FILES = ['./', './index.html', './tool.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
